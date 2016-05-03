a32d0b5dfb8f36ce1cc791b978b8650e4868b1ee
wyr8845 <wyr527572059@qq.com>
2016-05-03 13:40:12 +0800

    上传两个js文件
    一个获得第二大的数字
    一个反转字符串并输出

---
+++ getSecond.js
@@ -0,0 +1,20 @@
+﻿function getSecond(arr){ //返回数组中第二大的数，注意边界条件
+  var first;
+  var second;
+  
+  if (arr.constructor != Array)
+    return undefined;
+  
+  first = arr[0];
+  
+  for (var i = 1; i < arr.length; i++) {
+    if (arr[i] > first) {
+      second = first;
+      first = arr[i];
+    } else if (arr[i] > second) {
+      second = arr[i];
+    }
+  }
+  
+  return second;
+}
---
+++ reverse.js
@@ -0,0 +1,10 @@
+﻿function reverse(str){ //返回逆序字符串
+  var t = "";
+  var s = str.split("");
+  
+  for (var i = 0; i < s.length; i++) {
+    t += s[str.length - 1 - i];
+  }
+  
+  return t;
+}
