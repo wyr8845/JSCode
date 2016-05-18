7e1678b3b723b91511bfa4102947406fdd87840e
wyr8845 <wyr527572059@qq.com>
2016-05-18 14:11:53 +0800

    洗牌算法

---
+++ shuffle.js
@@ -0,0 +1,13 @@
+﻿function shuffle(arr){ //实现均匀洗牌
+	var num;
+	var temp;
+	
+	for (var i = 0; i < arr.length; i++) {
+		num = Math.floor(Math.random() * (arr.length - i));
+		temp = arr[arr.length - i - 1];
+		arr[arr.length - i - 1] = arr[num];
+		arr[num] = temp;
+	}
+	
+ 	return arr;
+}
