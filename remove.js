7b907a7f27cf703a249f54cb6b4e0f16bd242d3d
wyr8845 <wyr527572059@qq.com>
2016-05-18 21:22:26 +0800

    删除指定元素

---
+++ remove.js
@@ -0,0 +1,31 @@
+﻿function f() {
+	
+	//document.getElementById("out").innerHTML = sum(1,2,3,4);
+	Object.defineProperty(Array.prototype, 'remove', {
+		enumerable: false,
+	  	value: function(){ //删除一个或多个元素
+	  		var count = 0;
+	  		var i = 0;
+	  		var arrr = new Array();
+	  		
+	  		while (arguments.length > 0) {
+	  			if (arguments[i] == this[count]) {
+	  				this.splice(count, 1);
+	  				arrr.push(arguments[i]);
+	  				i++;
+	  			} else if (count < this.length) {
+	  				count++;
+	  			} else {
+	  				break;
+	  			}
+	  		}
+	  		
+	  		return arrr;
+	    }
+	});
+	
+	var arr = [1, 1, 1, 2, 2, 3, 4, 5];
+	var ttt = arr.remove(1, 1, 2, 4, 6);
+	console.log(arr);
+	console.log(ttt);
+}
