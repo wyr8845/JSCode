26f7c3e934470b426e150f452da78316056f8277
wyr8845 <wyr527572059@qq.com>
2016-05-18 14:52:36 +0800

    深拷贝
    完全复制一个对象

---
+++ copy.js
@@ -0,0 +1,11 @@
+﻿function copy(obj){  //深拷贝
+    function Clone(){}  
+    Clone.prototype = obj;  
+    var o = new Clone();  
+    for(var i in o){  
+        if(typeof o[i] == "object") {  
+            o[i] = copy(o[i]);  
+        }  
+    }  
+    return o;  
+}  
