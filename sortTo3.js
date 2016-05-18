2a3ce1a0d447f987d524ad83a7cb78fa449cd57d
wyr8845 <wyr527572059@qq.com>
2016-05-18 15:02:11 +0800

    越接近3的排在越前面

---
+++ sortTo3.js
@@ -0,0 +1,15 @@
+﻿function sort(arr){
+  var temp;
+  
+  for (var i = 0; i < arr.length - 1; i++) {
+    for (var j = i + 1; j < arr.length; j++) {
+      if (Math.abs(3 - arr[i]) > Math.abs(3 - arr[j])) {
+        temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+      }
+    }
+  }
+  
+  return arr;
+}
