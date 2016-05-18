0c7a8a6676e3e44132c2fcea92f384e779a76bbc
wyr8845 <wyr527572059@qq.com>
2016-05-18 19:56:33 +0800

    对若干的个数字进行求和

---
+++ sum.js
@@ -0,0 +1,10 @@
+﻿function sum(){
+    if (typeof arguments[0] == 'number')
+        var result = 0;
+    else
+        var result = '';
+    for (var i = 0; i < arguments.length; i++) {
+        result += arguments[i];
+    }
+    return result;
+}
