c5a104d44fee0d8f399fe65dd483b0ddd721d02d
wyr8845 <wyr527572059@qq.com>
2016-05-18 09:30:00 +0800

    最大奇数最小偶数

---
+++ find.js
@@ -0,0 +1,24 @@
+﻿function find(arr){
+  var BiggestOdd = undefined;
+  var leastEven = undefined;
+  
+  for (var i = 0; i < arr.length; i++) {
+    if (arr[i] % 2 == 1) {
+      BiggestOdd = arr[i];
+    } else if (arr[i] % 2 == 0) {
+      leastEven = arr[i];
+    }
+  }
+  
+  if (BiggestOdd == undefined || leastEven == undefined)
+    return undefined;
+  
+  for (var i = 0; i < arr.length; i++) {
+    if (arr[i] > BiggestOdd && arr[i] % 2 == 1)
+      BiggestOdd = arr[i];
+    else if (arr[i] < leastEven && arr[i] % 2 == 0)
+      leastEven = arr[i];
+  }
+  
+  return BiggestOdd + leastEven;
+}
