ab98e216785f30829dae6819ec6b5be7cd76849b
wyr8845 <wyr527572059@qq.com>
2016-05-18 14:24:22 +0800

    数组找结果
    传入一个数组，得到其中任意两个数之和为10的所有结果。

---
+++ findSumTen.js
@@ -0,0 +1,15 @@
+﻿function find(arr){
+  var res = new Array();
+  var k = 0;
+  
+  for (var i = 0; i < arr.length - 1; i++)
+    for (var j = i + 1; j < arr.length; j++)
+      if (arr[i] + arr[j] == 10) {
+        res[k] = new Array();
+        res[k][0] = arr[i];
+        res[k][1] = arr[j];
+        k++;
+      }
+  
+  return res;
+}
