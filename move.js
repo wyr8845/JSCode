31589545d0234a76162a77b4ea036fe90dcb40b2
wyr8845 <wyr527572059@qq.com>
2016-05-19 09:51:00 +0800

    匀速直线运动

---
+++ move.js
@@ -0,0 +1,14 @@
+﻿function move(x, y){ //请输入实现代码，平移请使用 translateX 实现
+  var block = document.getElementById('block-le01-p10');
+	var start = Date.now();
+	var dur = 2000;
+	requestAnimationFrame(function ff() {
+		var p = (Date.now() - start) / dur;
+		if (p < 1) {
+			block.style.left= 100 * p + 'px';
+			requestAnimationFrame(ff);
+		} else {
+			block.style.left= '100px';
+		}
+	});
+}
