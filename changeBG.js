ab94fb2477142c9ed1f1a4a6c63d4cf999124f42
wyr8845 <wyr527572059@qq.com>
2016-05-19 08:51:40 +0800

    实现一个背景变化的函数

---
+++ changeBG.js
@@ -0,0 +1,17 @@
+﻿function changeBG() { //颜色请用 rgb(r,g,b) 设置
+	var block = document.getElementById('block-le01-8-1');
+	var start = Date.now();
+	var dur = 3000;
+	requestAnimationFrame(function ff() {
+		var p = (Date.now() - start) / dur;
+		if (p < 1) {
+			var r = Math.round(255 * (1 - p));
+			var g = Math.round(255 * p);
+			var b = 0;
+			block.style.backgroundColor = 'rgb(' + [r, g, b] + ')';
+			requestAnimationFrame(ff);
+		} else {
+			block.style.backgroundColor = 'rgb(0,255,0)';
+		}
+	});
+}
