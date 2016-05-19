bb6283fa13ec1275d3008e8c4b34b67d2945ea4b
wyr8845 <wyr527572059@qq.com>
2016-05-19 15:38:33 +0800

    更改函数

---
+++ multicast.js
@@ -0,0 +1,16 @@
+﻿function multicast(func) {
+	return function(arr, ad) {
+		var len = arr.length;
+		var arResult = [];
+    var result = 0;
+    if (len > 0) {
+      for (var i = 0; i < len; i++) {
+        arResult.push(func(arr[i], ad));
+      }
+		  return arResult;
+    } else {
+      result = arguments[0] + arguments[1];
+      return result;
+    }
+	}
+}
---
+++ reducer.js
@@ -0,0 +1,30 @@
+﻿function f() {
+	function reducer(func){
+	    return function (){
+	      var ar = Array.prototype.slice.call(arguments);
+	      return ar.reduce(func);
+	    }
+	}
+	
+	function add(x, y){
+	    return x + y;
+	}
+	function mul(x, y){
+	    return x * y;
+	}
+	function concat(x, y){
+	    return x.concat(y);
+	}
+	
+	var add_many = reducer(add);
+	var mul_many = reducer(mul);
+	var concat_many = reducer(concat);
+	
+	add_many(1,2,3,4); //10
+	mul_many(1,2,3,4); //24
+	concat_many([1,2,3],[4,5],[6,7,8]); //[1,2,3,4,5,6,7,8]
+	
+	console.log(add_many(1,2,3,4,5,6));
+	console.log(mul_many(1,2,3,4));
+	console.log(concat_many([1,2,3,4],[4,5,6,7]));
+}
