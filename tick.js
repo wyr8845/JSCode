62239250184646f5489981096c117c970edde3d4
wyr8845 <wyr527572059@qq.com>
2016-05-19 09:26:58 +0800

    让 tick(10) 依次异步输出 [0,1,2,3,4,5,6,7,8,9]

---
+++ tick.js
@@ -0,0 +1,13 @@
+﻿var res = [];
+function tick(n){
+	var count = 0;
+	for(var i = 0; i < n; i++){
+    	setTimeout(function(){
+    		
+        	res.push(count);
+        	//console.log(res.slice(-1));
+        	count++;
+        	return res.slice(-1);
+        }, i * 50);
+    }
+}
